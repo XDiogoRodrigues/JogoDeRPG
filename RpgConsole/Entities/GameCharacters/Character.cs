8a120f15b1c6ddc61e79af9225788462af977c73
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-01-21 00:30:22 -0300

    Declarada a classe Character com seus atributos e metodo construtor

---
+++ RpgConsole/Entities/GameCharacters/Character.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgConsole.Entities.GameCharacters
+{
+    abstract class Character
+    {
+        public string Name { get; private set; }
+        public int Lv { get; private set; }
+        public int Hp { get; private set; }
+        public int Mp { get; private set; }
+        public int Strength { get; private set; }
+        public int AttackPower { get; private set; }
+        public int MagicPower {get; private set;}
+        public Weapon CharacterWeapon { get; private set; }
+
+        public Character(string name, int lv, int hp, int mp, int strength, int attackPower, int magicPower)
+        {
+            Name = name;
+            Lv = lv;
+            Hp = hp;
+            Mp = mp;
+            Strength = strength;
+            AttackPower = attackPower;
+            MagicPower = magicPower;
+        }
+    }
+}
