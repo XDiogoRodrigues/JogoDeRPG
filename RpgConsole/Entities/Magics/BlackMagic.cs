b6be02974db321de716dcaa54a9a37645bcf769e
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-01-24 05:59:36 -0300

    Declarada a classe Magic abstrata, e as classes WhiteMagic e BlackMagic com seus atributos e metodo construtor

---
+++ RpgConsole/Entities/Magics/BlackMagic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgConsole.Entities.Magics
+{
+    class BlackMagic: Magic
+    {
+        public int DamageMagic { get; set; }
+        public string ElementMagic { get; set; }
+
+        public BlackMagic(string typeMagic, string name, int costMana, int damageMagic, string elementMagic):base(typeMagic, name, costMana)
+        {
+
+            DamageMagic = damageMagic;
+            ElementMagic = elementMagic;
+        }
+    }
+}
---
+++ RpgConsole/Entities/Magics/WhiteMagic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgConsole.Entities.Magics
+{
+    class WhiteMagic: Magic
+    {
+        public int EffectMagic { get; set; }
+
+        public WhiteMagic(string typeMagic, string name, int costMana, int effectMagic):base(typeMagic, name, costMana)
+        {
+            EffectMagic = effectMagic;
+        }
+    }
+}
